fe491ae97516c23bb1d10db9fb9d86acb90f35ef
DanielFM99 <daniel_fm99@hotmail.com>
2021-03-13 14:12:55 -0300

    final

--- main.py
+++ main.py
@@ -5,26 +5,23 @@ import re
 
 app = Flask(__name__)
 
-# Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key = 'your secret key'
 
-# Enter your database connection details below
+# Database connection details
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '123456'
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_DB'] = 'coffebean'
 
 # Intialize MySQL
 mysql = MySQL(app)
 
-# http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
-@app.route('/pythonlogin/', methods=['GET', 'POST'])
+# http://localhost:5000/coffebean/ - Login page
+@app.route('/coffebean/', methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
     msg = ''
     # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        # Create variables for easy access
         password = request.form['password']
         email    = request.form['email']
         # Check if account exists using MySQL
@@ -40,32 +37,28 @@ def login():
             session['username'] = account['username']
             session['email'] = account['email']
             # Redirect to home page
-            #return 'Logged in successfully!'
             return redirect(url_for('home'))
         else:
-            # Account doesnt exist or username/password incorrect
+            # Account doesn't exist or username/password are incorrect
             msg = 'Incorrect email/password!'
-    # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 
-# http://localhost:5000/python/logout - this will be the logout page
-@app.route('/pythonlogin/logout')
+# http://localhost:5000/coffebean/logout - Logout page
+@app.route('/coffebean/logout')
 def logout():
-    # Remove session data, this will log the user out
+    # Remove session data
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
    # Redirect to login page
    return redirect(url_for('login'))
 
-# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
-@app.route('/pythonlogin/register', methods=['GET', 'POST'])
+# http://localhost:5000/coffebean/register - Registration page
+@app.route('/coffebean/register', methods=['GET', 'POST'])
 def register():
-    # Output message if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    # Check if "username", "password" and "email" POST requests exists
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
         username = request.form['username']
         usernameLen = len(username)
         password = request.form['password']
@@ -74,48 +67,47 @@ def register():
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE email = %s', (email,))
         account = cursor.fetchone()
-        # If account exists show error and validation checks
+        # If account exists, show error and validation checks
         if account:
             msg = 'Account already exists!'
+        # RegEx for email, back-end validation
         elif not re.match(r"(^[-\w\D][^@]{0,64}@([\w-]+\.)+[\w-]+$)", email):
             msg = 'Invalid email address!'
         elif not (usernameLen >= 5 and usernameLen <= 128):
             msg = 'Invalid username!'
         elif not username or not password or not email:
             msg = 'Please fill out the form!'
+        # RegEx for password, back-end validation
         elif not re.match(r"^(?=(?:.*[a-z]))(?=(?:.*[A-Z]){2})(?=(?:.*\d){2})(?=(?:.*[-@!$%^#&*()_+|~={}\[\]:\";`'<>?,.\/\\]){2}).{10,128}$", password):
             msg = 'Invalid password!'
         else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            # Account doesn't exists and the form is valid
             cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
-        # Form is empty... (no POST data)
+        # Form is empty
         msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
+    # Show registration form with message
     return render_template('register.html', msg=msg)
 
-# http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for loggedin users
-@app.route('/pythonlogin/home')
+# http://localhost:5000/coffebean/home - Home page
+@app.route('/coffebean/home')
 def home():
     # Check if user is loggedin
     if 'loggedin' in session:
-        # User is loggedin show them the home page
         return render_template('home.html', username=session['username'], email=session['email'],)
-    # User is not loggedin redirect to login page
+    # Redirect to login page
     return redirect(url_for('login'))
 
-# http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
-@app.route('/pythonlogin/profile')
+# http://localhost:5000/coffebean/profile - Profile page
+@app.route('/coffebean/profile')
 def profile():
-    # Check if user is loggedin
     if 'loggedin' in session:
-        # We need all the account info for the user so we can display it on the profile page
+        # Get session data
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
         account = cursor.fetchone()
         # Show the profile page with account info
         return render_template('profile.html', account=account)
-    # User is not loggedin redirect to login page
     return redirect(url_for('login'))
